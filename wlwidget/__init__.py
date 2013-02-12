d89ba24b005bd8be2de8ea8f7f8226bf72334260
Pablo Orduña <pablo.orduna@deusto.es>
2013-02-12 17:50:58 +0100

    Make the system work in other WebLab-Deusto server

--- wlwidget/__init__.py
+++ wlwidget/__init__.py
@@ -6,7 +6,7 @@ import json
 from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
-if os.uname()[1] == 'plunder':
+if os.uname()[1] in ('plunder','scabb'): # Deusto servers
     print "Installing proxy handler...",
     import urllib2
     proxy = urllib2.ProxyHandler({'http': 'http://proxy-s-priv.deusto.es:3128/'})
@@ -28,25 +28,8 @@ def main():
     except Exception as e:
         return "Error: %s" % e
 
-@app.route('/weblab-main/')
-def weblab_main():
-    st = request.args.get('st') or ''
-    try:
-        content = urllib2.urlopen("http://shindig.epfl.ch/rest/people/@me/@self?st=%s" % st).read()
-        content = json.loads(content)
-        
-        name = (content['entry'].get('displayName') or 'anonymous')
-        user_id = content['entry'].get('id') or 'no-id'
-        return render_template("contents.html", name = name, id = user_id)
-    except Exception as e:
-        return "Error: %s" % e
-   
-
-@app.route("/weblab-widget.xml")
-def widget():
-    return render_template('widget.xml', url = url_for('weblab_main', _external=True))
-
 @app.route("/widget.xml")
+@app.route("/widget2.xml")
 def widget():
     return render_template('widget.xml', url = url_for('main', _external=True))
 
