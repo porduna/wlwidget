eace5fd1e76b899cb10af0cd4d8a0bf71f6e0cfc
Pablo Orduña <pablo.orduna@deusto.es>
2013-02-12 17:35:39 +0100

    Add other links

--- wlwidget/__init__.py
+++ wlwidget/__init__.py
@@ -3,7 +3,7 @@ import os
 import urllib2
 import json
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 if os.uname()[1] == 'plunder':
@@ -21,14 +21,34 @@ def main():
     try:
         content = urllib2.urlopen("http://shindig.epfl.ch/rest/people/@me/@self?st=%s" % st).read()
         content = json.loads(content)
+        
         name = (content['entry'].get('displayName') or 'anonymous')
-        return render_template("contents.html", name = name)
+        user_id = content['entry'].get('id') or 'no-id'
+        return render_template("contents.html", name = name, id = user_id)
     except Exception as e:
         return "Error: %s" % e
 
+@app.route('/weblab-main/')
+def weblab_main():
+    st = request.args.get('st') or ''
+    try:
+        content = urllib2.urlopen("http://shindig.epfl.ch/rest/people/@me/@self?st=%s" % st).read()
+        content = json.loads(content)
+        
+        name = (content['entry'].get('displayName') or 'anonymous')
+        user_id = content['entry'].get('id') or 'no-id'
+        return render_template("contents.html", name = name, id = user_id)
+    except Exception as e:
+        return "Error: %s" % e
+   
+
+@app.route("/weblab-widget.xml")
+def widget():
+    return render_template('widget.xml', url = url_for('weblab_main', _external=True))
+
 @app.route("/widget.xml")
 def widget():
-    return render_template('widget.xml')
+    return render_template('widget.xml', url = url_for('main', _external=True))
 
 @app.route('/')
 def index():
